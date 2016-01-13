cd84ad6212179cd5c2becad19f4df4951560c48d
bobans@ <bobans@inbox.ru>
2016-01-13 17:37:07 +0300

    update from job

--- src/main/java/ru/round/fxController/mainController.java
+++ src/main/java/ru/round/fxController/mainController.java
@@ -1,19 +1,19 @@
 package ru.round.fxController;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import ru.round.startApp;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
-import javafx.scene.layout.AnchorPane;
+import javafx.geometry.*;
+import javafx.stage.*;
+import javafx.scene.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import ru.round.startApp;
+import javafx.scene.layout.BorderPane;
+
 
 import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.net.URL;
 import java.util.Optional;
@@ -33,6 +33,7 @@ public class mainController implements Initializable {
     public Button btnOpenFile;
     public MenuBar mainMenuBar;
     final String os = System.getProperty("os.name");
+    private Stage dialogStage;
 
 
     @Override
@@ -50,9 +51,48 @@ public class mainController implements Initializable {
         System.exit(0);
     }
 
+    public void ClickOnMenuAbout(ActionEvent actionEvent) {
+        System.out.printf("click on ClickOnMenuAbout");
+
+        dialogStage = new Stage();
+        BorderPane bPane = new BorderPane();
+
+        Scene scene =new Scene(bPane,300,150);
+        dialogStage.setScene(scene);
+        dialogStage.setTitle("О программе");
+        dialogStage.setResizable(false);
+        dialogStage.setMaximized(false);
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+
+        Label text_on_dialog = new Label("О программе О программе О программе О программе О программе О программе О программе О программе О программе О программе О программе О программе О программе ");
+        text_on_dialog.setWrapText(true);
+        bPane.setCenter(text_on_dialog);
+        bPane.setPadding(new Insets(15));
+
+
+        Button btn_close = new Button("Закрыть");
+        HBox hbox = new HBox(5);
+        hbox.setPadding(new Insets(5));
+        hbox.getChildren().addAll(btn_close);
+        hbox.setAlignment(Pos.BOTTOM_RIGHT);
+        bPane.setBottom(hbox);
+
+
+
+
+        btn_close.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                dialogStage.hide();
+            }
+        });
+        dialogStage.show();
+
+    }
+
 
     public void ClickOnbtn1(ActionEvent actionEvent) {
-        Alert  alert = new Alert(AlertType.INFORMATION,"tesxt");
+        Alert alert = new Alert(AlertType.INFORMATION,"tesxt");
         Optional<ButtonType> result = alert.showAndWait();
         if (result.isPresent() && result.get() == ButtonType.OK) {
         }
