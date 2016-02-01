83b519c14df10d9d640fc477d6abdf28e57f1ba6
bobans <bobans@inbox.ru>
2016-02-01 21:07:29 +0300

    changes on home check correct Base64ToText

--- src/main/java/ru/round/fxController/base64ConverterController.java
+++ src/main/java/ru/round/fxController/base64ConverterController.java
@@ -8,7 +8,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
+import javax.xml.bind.DatatypeConverter;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.ResourceBundle;
 
@@ -33,22 +35,26 @@ public class base64ConverterController implements Initializable {
 
     public void ClickOnBtnToPlainText(ActionEvent actionEvent) {
         try {
-            String base64Str = Base64.getEncoder().encodeToString(TextBase64Box.getText().getBytes());
+            //String base64Str = Base64.getEncoder().withoutPadding().encodeToString(TextBase64Box.getText().getBytes());
+            String base64Str = Base64.getEncoder().encodeToString(TextBase64Box.getText().getBytes(StandardCharsets.UTF_8));
             System.out.println(base64Str);
             TextPlainBox.setText(base64Str);
         } catch (Exception ex) {
-            Alert alert = new Alert(Alert.AlertType.ERROR,"Произошла непредвиденная ошибка: " + ex);
+            Alert alert = new Alert(Alert.AlertType.ERROR,"ClickOnBtnToPlainText | Произошла непредвиденная ошибка: " + ex);
             alert.show();
         }
     }
 
     public void ClickOnBtnToBase64(ActionEvent actionEvent) {
         try {
-            byte[] base64Bytes = Base64.getDecoder().decode(TextPlainBox.getText());
+            byte[] base64Bytes = DatatypeConverter.parseBase64Binary(TextPlainBox.getText());
+            TextBase64Box.setText(new String(base64Bytes,StandardCharsets.UTF_8));
             System.out.println(new String(base64Bytes));
-            TextBase64Box.setText(new String(base64Bytes));
+            /*byte[] base64Bytes = Base64.getDecoder().decode(TextPlainBox.getText());
+            System.out.println(new String(base64Bytes));
+            TextBase64Box.setText(new String(base64Bytes,StandardCharsets.UTF_8));*/
         } catch (Exception ex) {
-            Alert alert = new Alert(Alert.AlertType.ERROR,"Произошла непредвиденная ошибка: " + ex);
+            Alert alert = new Alert(Alert.AlertType.ERROR,"ClickOnBtnToBase64 | Произошла непредвиденная ошибка: " + ex);
             alert.show();
     }
 
